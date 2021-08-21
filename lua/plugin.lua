1bd17aa1f92487c803ed449beb94c93097d3498d
leon332157 <leon332157@gmail.com>
2021-08-20 22:02:02 -0400

    fix format

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -21,32 +21,31 @@ return require('packer').startup(function()
         requires = 'kyazdani42/nvim-web-devicons'
     }
     use {
-
         'nvim-telescope/telescope.nvim',
         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
         config = function()
-            require("pluginConfig.telescope")
+            require('pluginConfig.telescope')
         end
     }
-    use "Pocco81/AutoSave.nvim"
-    use "glepnir/dashboard-nvim"
-    use "lukas-reineke/indent-blankline.nvim"
+    use 'Pocco81/AutoSave.nvim'
+    use 'glepnir/dashboard-nvim'
+    use 'lukas-reineke/indent-blankline.nvim'
     use 'Mofiqul/vscode.nvim' -- VSCode color theme
     use 'kaicataldo/material.vim' -- Material Theme
-    use 'fratajczak/one-monokai-vim' -- Monokai 
+    use 'fratajczak/one-monokai-vim' -- Monokai
     use {
         'lewis6991/gitsigns.nvim',
         requires = {'nvim-lua/plenary.nvim'}
     }
     use 'shaunsingh/moonlight.nvim'
-    use "rcarriga/nvim-notify"
+    use 'rcarriga/nvim-notify'
     use 'sainnhe/gruvbox-material'
     use 'vim-airline/vim-airline'
-use {
-			"nvim-treesitter/nvim-treesitter",
-			run = ":TSUpdate",
-			config = function()
-				require("pluginConfig.treesitter")
-			end
-		}
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        config = function()
+            require('pluginConfig.treesitter')
+        end
+    }
 end)
