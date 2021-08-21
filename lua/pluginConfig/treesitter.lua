d86025c699c604fc6e675c4d2bafb31d8112e949
leon332157 <leon332157@gmail.com>
2021-08-20 22:11:17 -0400

    add treesitter config

---
+++ lua/pluginConfig/treesitter.lua
@@ -0,0 +1,52 @@
+
+require("nvim-treesitter.configs").setup({
+    ensure_installed = "maintained",
+    highlight = {
+        enable = true,
+        use_languagetree = true
+    },
+	indent = {
+		enable = true
+	},
+	autopairs = { enable = true },
+	context_commentstring = {
+		enable = true
+	},
+	textsubjects = {
+		enable = true,
+		keymaps = {
+			['.'] = 'textsubjects-smart',
+			[';'] = 'textsubjects-container-outer',
+		}
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["ab"] = "@block.outer",
+				["ib"] = "@block.inner",
+				["ap"] = "@parameter.outer",
+				["ip"] = "@parameter.inner",
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["al"] = "@loop.outer",
+				["il"] = "@loop.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>a"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>A"] = "@parameter.inner",
+			},
+		},
+	},
+})
+
